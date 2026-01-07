ca8f93f2c8215101a3abb738317ee5f8b13ba944
Bimal Thapa <bimalthapa2062@gmail.com>
2026-01-07 22:28:27 +0545

    initiallization complete

---
+++ rough.py
@@ -0,0 +1,13 @@
+memory=[0]*5
+print(memory)
+
+y=0x200
+print(y)
+
+b= [[0]*64 for _ in range(32)]  #use to create a display of 64 by 32 pixal 
+print(b)
+
+ar=[1,2,3,4,5,6]
+ 
+for i ,val in enumerate(ar):
+    print(i)
