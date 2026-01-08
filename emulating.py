643a1e4bae8e98677e3197916500dd156a9b7f6b
Bimal Thapa <bimalthapa2062@gmail.com>
2026-01-08 17:40:55 +0545

    cpu cycle samma vako xa

--- emulating.py
+++ emulating.py
@@ -47,11 +47,55 @@ class chip8:
         for i, val in enumerate(rom_data):
             self.memory[0x200 +i]=val
 
+
     def opcode_fetch(self):
         opcode=self.memory[self.pc]<<8 | self.memory[self.pc +1]
         return opcode
     
-    #def decode_excute(self,opcode):
+    def decode_excute(self,opcode):
+        if opcode == 0x00E0:  #clear the screen
+            self.display=[[0]*64 for _ in range(32)]
+            print("clear the screen")
+
+    def cpu_cycle(self):  #main cpu cycle
+        opcode=self.opcode_fetch()
+        self.decode_excute(opcode)
+
+        if self.delay_timer > 0:
+            self.delay_timer -=1
+             #ya sund timer ralxu
+
+        elif opcode == 0x00EE: #returnig from the subroutine
+            self.pc=self.stack.pop()
+            print("return form sunroutine")
+
+        elif opcode & 0xF000 == 0x6000: #setting the register vx to nn
+            x=(opcode & 0x0F00)>>8
+            nn=opcode & 0x00FF
+            self.V[x]=nn
+        
+        elif opcode & 0xF000 == 0x7000: #adding nn to register vx
+            x=(opcode & 0x0F00)>>8
+            nn=opcode & 0x00FF
+            self.v[x]=(self.V[x]+nn) & 0xFF
+
+        elif opcode & 0xF000 == 0xA000:
+            address=opcode & 0x0FFF
+            self.I=address
+            return # doing so the 12 bit adress is stored in i register therefore we dont need to increment pc by2
+
+
+        elif opcode & 0xF000 == 0x1000:
+            address=opcode & 0x0FFF
+            self.pc=address
+            return
+        else:
+            print(f"unknown opcode: {hex(opcode)}")
+
+        self.pc +=2
+
+        
+            
 
 emu =chip8()
 emu.load_rom("rom_data.ch8")
--- rough.py
+++ rough.py
@@ -2,3 +2,6 @@ b=0b00000010
 c=0b100
 d= b|c
 print(bin(d))
+
+stack=[1,2,3,4,5]
+print(stack.pop())
